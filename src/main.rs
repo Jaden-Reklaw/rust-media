7b199f52c9a8d4af5052019e0e2278eb1f45f364
Jaden-Reklaw <jwalker117@gmail.com>
2024-10-16 21:57:14 -0500

    finished lesson on enums using pattern matching

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,28 @@ enum Media {
     Audio { title: String, artist: String },
 }
 
+impl Media {
+    fn description(&self) -> String {
+        // This is how you would do it with if let
+        // if let Media::Book { title, author } = self {
+        //     format!("Book: {} by {}", title, author)
+        // } else if let Media::Movie { title, director } = self {
+        //     format!("Movie: {} by {}", title, director)
+        // } else if let Media::Audio { title, artist } = self {
+        //     format!("Audio: {} by {}", title, artist)
+        // } else {
+        //     panic!("This should never happen")
+        // }
+
+        // match is how to tell what self is
+        match self {
+            Media::Book { title, author } => format!("Book: {} by {}", title, author),
+            Media::Movie { title, director } => format!("Movie: {} by {}", title, director),
+            Media::Audio { title, artist } => format!("Audio: {} by {}", title, artist),
+        }
+    }
+}
+
 // Implementing a method for the enum allows you to run it on books, movies, and audio
 fn print_media(media: Media) {
     println!("{:#?}", media);
@@ -26,7 +48,11 @@ fn main() {
     };
 
     // behold the power of enums
-    print_media(book);
-    print_media(movie);
-    print_media(audio);
+    // print_media(book);
+    // print_media(movie);
+    // print_media(audio);
+
+    println!("{}", book.description());
+    println!("{}", movie.description());
+    println!("{}", audio.description());
 }
