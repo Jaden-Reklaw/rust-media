b19c7eda861d0f2b71758876d6139314baa48db0
Jaden-Reklaw <jwalker117@gmail.com>
2024-10-21 22:37:00 -0500

    completed exercise working with options.

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,31 @@
-
-// Think of enums like making multiple structs but not
-#[derive(Debug)]
-enum Media {
-    Book { title: String, author: String },
-    Movie { title: String, director: String },
-    Audio { title: String, artist: String },
-    Podcast(u32), //You can do this 
-    Placeholder, // or this
-}
-
-impl Media {
-    fn description(&self) -> String {
-        
-        match self {
-            Media::Book { title, author } => format!("Book: {} by {}", title, author),
-            Media::Movie { title, director } => format!("Movie: {} by {}", title, director),
-            Media::Audio { title, artist } => format!("Audio: {} by {}", title, artist),
-            Media::Podcast(episodes) => format!("Podcast with {} episodes", episodes),
-            Media::Placeholder => format!("Placeholder"),
-        }
-    }
-}
+// TODO:
+// 1) Safely access the first account in the 'accounts' vector using the 
+//    .first_mut() method. 
+// 2) '.first_mut()' returns an Option whose Some variant is a mutable ref to 
+//     an Account. Use a 'match' statement to figure out if
+//     you have a Some or a None
+// 3) In the Some case, set the balance of the account to 30, then print the account
+// 4) In the None case, print the message "No account found"
+// Hint: You might have to add in the 'mut' keyword somewhere...
 
 #[derive(Debug)]
-struct Catalog {
-    items: Vec<Media>,
-}
-
-impl Catalog {
-    fn new() -> Self {
-        Catalog { items: Vec::new() }
-    }
-
-    fn add(&mut self, item: Media) {
-        self.items.push(item);
-    }
-
-    fn get_at_index(&self, index: usize) -> Option<&Media> {
-        if self.items.len() > index {
-            Some(&self.items[index])
-        } else {
-            None
-        } 
-    }
+struct Account {
+    balance: i32
 }
 
 fn main() {
-
-    let book = Media::Book {
-        title: "The Hobbit".to_string(),
-        author: "J.R.R. Tolkien".to_string()
-    };
-    let movie = Media::Movie {
-        title: "The Lord of the Rings".to_string(),
-        director: "Peter Jackson".to_string()
-    };
-    let audio = Media::Audio {
-        title: "The Beatles".to_string(),
-        artist: "The Beatles".to_string()
-    };
-    let podcast = Media::Podcast(100);
-    let placeholder = Media::Placeholder;
-
-
-
-    let mut catalog = Catalog::new();
-    catalog.add(book);
-    catalog.add(movie);
-    catalog.add(audio);
-    catalog.add(podcast);
-    catalog.add(placeholder);
-
-    //Other ways to deal with options
-    //Using unwrap not used very often
-    println!("{:#?}", catalog.get_at_index(0).unwrap().description());
-    //println!("{:#?}", catalog.get_at_index(13).unwrap().description()); //This will panic 
-
-    //Using expect good to use for enviornment variables that you expect but should crash if they are not there
-    println!("{:#?}", catalog.get_at_index(0).expect("Index out of bounds").description());
-    //println!("{:#?}", catalog.get_at_index(13).expect("Index out of bounds").description()); //This will panic
-
-    //Using unwrap_or_else
-    println!("{:#?}", catalog.get_at_index(0).unwrap_or_else(|| &Media::Placeholder).description());
-    println!("{:#?}", catalog.get_at_index(13).unwrap_or_else(|| &Media::Placeholder).description());
-
+    let mut accounts: Vec<Account> = vec![
+        Account { balance: 0 },
+        Account { balance: 10 }
+    ];
+    
+    match accounts.first_mut() {
+        Some(account) => {
+            account.balance = 30;
+            println!("{:?}", account);
+        },
+        None => {
+            println!("No account found");
+        }
+    }
 }
